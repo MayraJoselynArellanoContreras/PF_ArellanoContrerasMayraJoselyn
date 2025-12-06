c611b95d3f76caa7330d22a1b3977ab1f8818ca4
contr <mayra.ac@tecjerez.edu.mx>
2025-12-06 15:32:02 -0600

    Estructura básica de la clase Pila

--- ProyectoFinal_ED/src/PruebaProyectoFinal.java
+++ ProyectoFinal_ED/src/PruebaProyectoFinal.java
@@ -1,4 +1,16 @@
 
+class Pila {
+	private String[] elementos;
+    private int tope;
+    private int capacidad;
+
+    public Pila(int capacidad) {
+        this.capacidad = capacidad;
+        this.elementos = new String[capacidad];
+        this.tope = -1;
+    }
+}
+
 public class PruebaProyectoFinal {
 
 	public static void main(String[] args) {
