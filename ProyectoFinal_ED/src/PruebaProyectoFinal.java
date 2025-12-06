11e2a4a41b41eeb94654d764afd8ced7cccb6763
contr <mayra.ac@tecjerez.edu.mx>
2025-12-06 16:39:02 -0600

    Metodos auxiliares para el algoritmo de ordenamiento QuickSort

--- ProyectoFinal_ED/src/PruebaProyectoFinal.java
+++ ProyectoFinal_ED/src/PruebaProyectoFinal.java
@@ -143,6 +143,37 @@ class ManipulacionArchivo {
     }
 	
 	
+//===== METODOS AUXILIARES PARA EL ALGORITMO QUICKSORT =====
+	private void quickSort(String[] arr, int bajo, int alto) {
+        if (bajo < alto) {
+            int pi = particion(arr, bajo, alto);
+            quickSort(arr, bajo, pi - 1);
+            quickSort(arr, pi + 1, alto);
+        }
+    }
+
+    private int particion(String[] arr, int bajo, int alto) {
+        String pivote = arr[alto];
+        int i = bajo - 1;
+
+        for (int j = bajo; j < alto; j++) {
+        	if (arr[j].toLowerCase().compareTo(pivote.toLowerCase()) <= 0) {
+                i++;
+                String temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        String temp = arr[i + 1];
+        arr[i + 1] = arr[alto];
+        arr[alto] = temp;
+
+        return i + 1;
+    }
+
+	
+	
 }
 public class PruebaProyectoFinal {
 
